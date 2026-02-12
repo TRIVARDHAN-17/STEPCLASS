f0b2d3c427cda05b4bf3e41a981547e36c1160a5
TRIVARDHAN <tp3615@srmist.edu.in>
2026-02-12 10:49:27 +0530

    2nd commit

---
+++ src/helloworld.java
@@ -0,0 +1,7 @@
+public class helloworld {
+    public static void main (String args[])
+    {
+        System.out.println("hellow my name is trivardhan i am from srm betch 2nd yrs  ");
+        System.out.println("hellow world");
+    }
+}
